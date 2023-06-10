8fdf41a2d03ae72cdbea249cc2196f107ad2ac50
NguyenVanNguyen03 <3zuilachinh3@gmail.com>
2023-06-10 12:07:45 +0700

    baits

---
+++ Variables/TheFortuneTeller.ts
@@ -0,0 +1,14 @@
+let fortuneTeller: {
+  personName: string;
+  numberOfChilden: number;
+  partnerName: string;
+  geographicLocation: string;
+  jobTitle: string;
+} = {
+  personName: "Clint Afton",
+  numberOfChilden: 2,
+  partnerName: "Vannessa Elton",
+  geographicLocation: "Wyoming",
+  jobTitle: "CyberSecurity Specialist",
+};
+console.log(`You are ${fortuneTeller.personName} and you will be a ${fortuneTeller.jobTitle} in ${fortuneTeller.geographicLocation}, you married to ${fortuneTeller.partnerName} with ${fortuneTeller.numberOfChilden} kids`);
